e17cfc04e30c1886045dc59c18b20b8687d3e3ee
wowufoundme <shubhkirti.work@gmail.com>
2018-03-09 11:21:41 +0530

    Major Update

---
+++ compiled/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.nav-toggle-container').click(function(){
+    $('#nav').toggleClass('is-active');
+    $('#toggle').toggleClass('active');
+  });
+  $('.popup-close').click(function(){
+    $('#popup').addClass('popup-closed');
+  });
+});
